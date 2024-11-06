5243d16421b9674f9191848bc69475eee7118ba6
coffee8cat <ierandil@yandex.ru>
2024-11-06 23:12:50 +0300

    dinamic dequeue + free colored

--- src/bin_tree.cpp
+++ src/bin_tree.cpp
@@ -39,15 +39,122 @@ int add_node(node_t* node, int data)
     return 0;
 }
 
-int print_tree(node_t* node)
+FILE* prepare_to_dump()
 {
+    system("make clean_data\n");
+    FILE* fp = fopen("list_dump.html", "w");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "ERROR: Cannot open html file fot dump\n");
+        return NULL;
+    }
+    fprintf(fp, "<pre>\n");
+
+    return fp;
+}
+
+int tree_dump(node_t* root, FILE* html_stream)
+{
+    static size_t dump_counter = 1;
+    char dot_file_name[BUFSIZ] = "";
+    char png_file_name[BUFSIZ] = "";
+    sprintf(dot_file_name, "data\\dump%d.dot", dump_counter);
+    sprintf(png_file_name, "data\\dump%d.png", dump_counter);
+
+    FILE* fp = fopen(dot_file_name, "w");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "ERROR: Unable to open file for dump\n");
+        return -1;
+    }
+
+    make_dot_file(root, fp);
+    fclose(fp);
+
+    char command[BUFSIZ] = "";
+    sprintf(command, "dot %s -Tpng -o %s\n", dot_file_name, png_file_name);
+    system(command);
+
+    dump_to_html(root, png_file_name, html_stream);
+
+    dump_counter++;
+    return 0;
+}
+
+int dump_to_html(node_t* root, char* png_file_name, FILE* html_stream)
+{
+    assert(root);
+    assert(png_file_name);
+
+    fprintf(html_stream, "MEMORY PRINT\n");
+    print_tree(root, html_stream);
+
+    fprintf(html_stream, "\n<img src=%s>\n", png_file_name);
+
+    return 0;
+}
+
+int make_dot_file(node_t* root, FILE* fp)
+{
+    assert(fp);
+    assert(root);
+
+    fprintf(fp, "digraph\n{\n"
+                "    rankdir=TP\n\n"
+                "    ");
+
+    write_node_info(root, fp);
+
+    fprintf(fp, "}");
+
+    printf("dot file completed\n");
+    return 0;
+}
+
+int write_node_info(node_t* node, FILE* fp)
+{
+    assert(fp);
+
     if (!node) { return 1; }
 
-    printf("{");
-    if (node -> left) { print_tree(node -> left); }
-    printf("%d", node -> data);
-    if (node -> right) { print_tree(node -> right); }
-    printf("}");
+    fprintf(fp, "    node%p[shape=record,style=\"rounded,filled\",fillcolor=\"#39CCCC\",label=\"{ ptr: %p | data: %d | { left: %p | right: %p }}\"];\n",
+            node, node, node -> data, node -> left, node -> right);
+
+    if (node -> left)
+    {
+        fprintf(fp, "    node%p -> node%p[color=\"#0855F0\"]\n", node, node -> left);
+        write_node_info(node -> left, fp);
+    }
+    if (node -> right)
+    {
+        fprintf(fp, "    node%p -> node%p[color=\"#0855F0\"]\n", node, node -> right);
+        write_node_info(node -> right, fp);
+    }
+
+    return 0;
+}
+
+int print_tree(node_t* node, FILE* stream)
+{
+    assert(stream);
+
+    if (!node) { return 1; }
+
+    fprintf(stream, "{");
+    if (node -> left)  { print_tree(node -> left, stream);  }
+    fprintf(stream, "%d", node -> data);
+    if (node -> right) { print_tree(node -> right, stream); }
+    fprintf(stream, "}");
+
+    return 0;
+}
+
+int tree_dtor(node_t* node)
+{
+    if (!node) { return 1; }
+    if (node -> left)  { tree_dtor(node -> left);  }
+    if (node -> right) { tree_dtor(node -> right); }
+    free(node);
 
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,22 @@
 
 int main()
 {
+    FILE* html_stream = prepare_to_dump();
+    if (html_stream == NULL)
+        return -1;
+
     node_t* root = new_node(50, NULL, NULL);
     if (!root) {return -1;}
     add_node(root, 30);
     add_node(root, 70);
     add_node(root, 40);
 
-    print_tree(root);
+    print_tree(root, stdout);
+    tree_dump(root, html_stream);
+    add_node(root, 12);
+    tree_dump(root, html_stream);
+
+    tree_dtor(root);
 
     return 0;
 }
